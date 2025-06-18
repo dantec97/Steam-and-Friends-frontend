bbb4532f02a6135c67df09e56f0f8099b9f830bb
dantec97 <dantecpriority@gmail.com>
2025-06-18 14:27:50 -0400

    Fixed protected routes, user needs valid token & steam_id for auth success

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,11 +1,12 @@
+// filepath: src/components/ProtectedRoute.jsx
 import { Navigate } from "react-router-dom";
 import React from "react";
 const ProtectedRoute = ({ children }) => {
   const steamId = localStorage.getItem("steam_id");
-  if (!steamId) {
+  const token = localStorage.getItem("token");
+  if (!steamId || !token) {
     return <Navigate to="/login" replace />;
   }
   return children;
 };
-
 export default ProtectedRoute;
