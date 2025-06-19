8e0a460cf287797e24fa361d5f4f6bcaa64bd265
dantec97 <dantecpriority@gmail.com>
2025-06-19 16:34:25 -0400

    Deployment #2 testing the full app

--- src/App.jsx
+++ src/App.jsx
@@ -1,126 +1,126 @@
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// // import Home from "./components/Home";
-// import Login from "./components/Login";
-// import MyGames from "./components/MyGames";
-// import Friends from "./components/Friends";
-// import FriendGames from "./components/FriendGames";
-// import GameComparison from "./components/GameComparison";
-// import Groups from "./components/Groups";
-// import GroupDetails from "./components/GroupDetails";    
-// import SteamAuthSuccess from "./components/SteamAuthSuccess";
-// import ProtectedRoute from "./components/ProtectedRoute";
-// import Dashboard from "./components/Dashboard";
-// import NotFound from "./components/NotFound";
-// import FriendsWithGame from "./components/FriendsWithGame";
-
-
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route
-//           path="/"
-//           element={
-//             <ProtectedRoute>
-//               <Dashboard />
-//             </ProtectedRoute>
-//           }
-//         />
-//         <Route path="/login" element={<Login />} />
-//         <Route
-//           path="/my_games"
-//           element={
-//             <ProtectedRoute>
-//               <MyGames />
-//             </ProtectedRoute>
-//           }
-//         />
-//         <Route
-//           path="/friends"
-//           element={
-//             <ProtectedRoute>
-//               <Friends />
-//             </ProtectedRoute>
-//           }
-//         />
-//         <Route
-//           path="/friends/games"
-//           element={
-//             <ProtectedRoute>
-//               <FriendGames />
-//             </ProtectedRoute>
-//           }
-//         />
-//         <Route
-//           path="/friends/:friendSteamId/games"
-//           element={
-//             <ProtectedRoute>
-//               <FriendGames />
-//             </ProtectedRoute>
-//           }
-//         />
-//         <Route
-//           path="/games/:gameId/comparison"
-//           element={
-//             <ProtectedRoute>
-//               <GameComparison />
-//             </ProtectedRoute>
-//           }
-//         />
-//         <Route
-//           path="/groups"
-//           element={
-//             <ProtectedRoute>
-//               <Groups />
-//             </ProtectedRoute>
-//           }
-//         />
-//         <Route
-//           path="/groups/:groupId"
-//           element={
-//             <ProtectedRoute>
-//               <GroupDetails />
-//             </ProtectedRoute>
-//           }
-//         />
-//         <Route
-//           path="/dashboard"
-//           element={
-//             <ProtectedRoute>
-//               <Dashboard />
-//             </ProtectedRoute>
-//           }
-//         />
-//         <Route path="/steam-auth-success" element={<SteamAuthSuccess />} />
-//         <Route
-//           path="/games/:gameId/friends"
-//           element={
-//             <ProtectedRoute>
-//               <FriendsWithGame />
-//             </ProtectedRoute>
-//           }
-//         />
-//         <Route path="*" element={<NotFound />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Home from "./components/Home";
+import Login from "./components/Login";
+import MyGames from "./components/MyGames";
+import Friends from "./components/Friends";
+import FriendGames from "./components/FriendGames";
+import GameComparison from "./components/GameComparison";
+import Groups from "./components/Groups";
+import GroupDetails from "./components/GroupDetails";    
 import SteamAuthSuccess from "./components/SteamAuthSuccess";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Dashboard from "./components/Dashboard";
+import NotFound from "./components/NotFound";
+import FriendsWithGame from "./components/FriendsWithGame";
+
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/my_games"
+          element={
+            <ProtectedRoute>
+              <MyGames />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/friends"
+          element={
+            <ProtectedRoute>
+              <Friends />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/friends/games"
+          element={
+            <ProtectedRoute>
+              <FriendGames />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/friends/:friendSteamId/games"
+          element={
+            <ProtectedRoute>
+              <FriendGames />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/games/:gameId/comparison"
+          element={
+            <ProtectedRoute>
+              <GameComparison />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/groups"
+          element={
+            <ProtectedRoute>
+              <Groups />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/groups/:groupId"
+          element={
+            <ProtectedRoute>
+              <GroupDetails />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
         <Route path="/steam-auth-success" element={<SteamAuthSuccess />} />
-        <Route path="*" element={<div>Not Found</div>} />
+        <Route
+          path="/games/:gameId/friends"
+          element={
+            <ProtectedRoute>
+              <FriendsWithGame />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
+
+//SIMPLE TEST APP FROM WHEN THINGS WERE NOT GOING SO WELL.....
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import SteamAuthSuccess from "./components/SteamAuthSuccess";
+
+// function App() {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/steam-auth-success" element={<SteamAuthSuccess />} />
+//         <Route path="*" element={<div>Not Found</div>} />
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App;
--- src/components/SteamAuthSuccess.jsx
+++ src/components/SteamAuthSuccess.jsx
@@ -1,36 +1,39 @@
 import { useEffect } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 
-// const SteamAuthSuccess = () => {
-//   const [params] = useSearchParams();
-//   const navigate = useNavigate();
-
-//   useEffect(() => {
-//     if (!params) {
-//       console.error("No search params found!");
-//       return;
-//     }
-//     const steamId = params.get("steam_id");
-//     const displayName = params.get("display_name");
-//     const avatarUrl = params.get("avatar_url");
-//     const token = params.get("token");
-//     console.log("Params:", steamId, token, displayName, avatarUrl);
-//     if (steamId && token) {
-//       localStorage.setItem("steam_id", steamId);
-//       localStorage.setItem("account_display_name", displayName);
-//       localStorage.setItem("avatar_url", avatarUrl);
-//       localStorage.setItem("token", token);
-//       navigate("/dashboard");
-//     } else {
-//       navigate("/login?error=steam_auth_failed");
-//     }
-//   }, [params, navigate]);
-
-//   return <div>Signing you in with Steam...</div>;
-// };
-
 const SteamAuthSuccess = () => {
-  return <div>SteamAuthSuccess is rendering!</div>;
+  const [params] = useSearchParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!params) {
+      console.error("No search params found!");
+      return;
+    }
+    const steamId = params.get("steam_id");
+    const displayName = params.get("display_name");
+    const avatarUrl = params.get("avatar_url");
+    const token = params.get("token");
+    console.log("Params:", steamId, token, displayName, avatarUrl);
+    if (steamId && token) {
+      localStorage.setItem("steam_id", steamId);
+      localStorage.setItem("account_display_name", displayName);
+      localStorage.setItem("avatar_url", avatarUrl);
+      localStorage.setItem("token", token);
+      navigate("/dashboard");
+    } else {
+      navigate("/login?error=steam_auth_failed");
+    }
+  }, [params, navigate]);
+
+  return <div>Signing you in with Steam...</div>;
 };
+
+
+
+//simple auth from when things were not going so well at all 
+// const SteamAuthSuccess = () => {
+//   return <div>SteamAuthSuccess is rendering!</div>;
+// };
 
 export default SteamAuthSuccess;
